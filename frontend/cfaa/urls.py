6d2f09bc2763b46c898572a341ac29337dc23023
FabioPernegger <fabio.pernegger@gmail.com>
2024-09-23 22:39:23 +0200

    Many new changes to design and report page

--- frontend/cfaa/urls.py
+++ frontend/cfaa/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('discovery/', views.discovery_page, name='discovery'),
     #path('discovery/<int:queryid>/', views.discovery_page, name='discovery'),
-    path('report/', views.report_page, name='report'),
+    path('report/<int:report_id>/', views.report_page, name='report'),
+    path('monitor-question/', views.monitor_question, name='monitor_question'),
 ]
